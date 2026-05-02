6ffd3b35aef3699411b2e4e99eb210cc7d604229
nirob <nhnirob362@gmail.com>
2026-05-02 22:00:29 +0600

    no found fix

--- src/lib/dataFetch.js
+++ src/lib/dataFetch.js
@@ -1,5 +1,5 @@
 export const allBooks=async()=>{
-    const res=await fetch('http://localhost:3000/bookData.json',{cache:"no-store"});
+    const res=await fetch('https://bookbounds.vercel.app/bookData.json',{cache:"no-store"});
     const data=await res.json();
     return data;
 }
