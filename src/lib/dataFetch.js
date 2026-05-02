e7bdc3f64649088186e4d2fa3266e2cadb3ed784
nirob <nhnirob362@gmail.com>
2026-05-02 21:54:13 +0600

    bug fix

--- src/lib/dataFetch.js
+++ src/lib/dataFetch.js
@@ -1,5 +1,5 @@
 export const allBooks=async()=>{
-    const res=await fetch('http://localhost:3000/bookData.json');
+    const res=await fetch('http://localhost:3000/bookData.json',{cache:"no-store"});
     const data=await res.json();
     return data;
 }
